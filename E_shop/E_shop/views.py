be05e3d782682c8b3fb9b35881b3287c69f968f1
HimanthaD4 <navanjanajayakodi1@gmail.com>
2024-01-03 08:07:01 +0530

    cart function added

--- E_shop/E_shop/views.py
+++ E_shop/E_shop/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render,redirect
 from store_app.models import Product,Categories,Filter_price,Contact_us
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
+
 
 def BASE(request):
     return render(request,'Main/base.html')
@@ -63,3 +68,89 @@ def Contact_Page(request):
         contact.save()
         return redirect('home')
     return render(request,'Main/contact.html')
+
+
+def HandleRegister(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        customer = User.objects.create_user(username,email,pass1)
+        customer.first_name = first_name
+        customer.last_name = last_name
+        customer.save()
+        return redirect('register')
+
+    return render(request,'Registration/auth.html')
+
+
+def HandleLogin(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username = username,password = password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return redirect('login')
+
+
+    return render(request, 'Registration/auth.html')
+
+
+def HandleLogout(request):
+    logout(request)
+
+    return redirect('home')
+
+
+
+
+@login_required(login_url="/login/")
+def cart_add(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("home")
+
+
+@login_required(login_url="/login/")
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/login/")
+def item_increment(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/login/")
+def item_decrement(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.decrement(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/login/")
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/login/")
+def cart_detail(request):
+    return render(request, 'Cart/cart_details.html')
