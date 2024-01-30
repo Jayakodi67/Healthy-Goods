0fbcac4862e35589fea46f4d38227a2a06719816
HimanthaD4 <152485458+Jayakodi67@users.noreply.github.com>
2024-01-30 20:20:06 +0530

    add catalog

--- E_shop/store_app/models.py
+++ E_shop/store_app/models.py
@@ -14,21 +14,6 @@ class Categories(models.Model):
         return self.name
 
 
-class Filter_price(models.Model):
-    FILTER_PRICE = (
-        ('0 TO 200','0 TO 200'),
-        ('200 TO 400', '200 TO 400'),
-        ('400 TO 600', '400 TO 600'),
-        ('600 TO 800', '600 TO 800'),
-        ('800 TO 1000', '800 TO 1000'),
-        ('1000 TO 10000', '1000 TO 10000'),
-    )
-    price = models.CharField(choices=FILTER_PRICE,max_length=60)
-
-    def __str__(self):
-        return self.price
-
-
 class Product(models.Model):
     CONDITION = (('GAP Certified','GAP Certified'),('No','No'))
     STOCK = ('IN STOCK','IN STOCK'),('OUT OF STOCK','OUT OF STOCK')
@@ -39,7 +24,6 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
     condition = models.CharField(choices=CONDITION,max_length=100)
-    # information = RichTextField(null=True)
     description = RichTextField(null=True)
     stock = models.CharField(choices=STOCK,max_length=200)
     status = models.CharField(choices=STATUS,max_length=200)
@@ -47,7 +31,6 @@ class Product(models.Model):
 
     categories = models.ForeignKey(Categories,on_delete=models.CASCADE)
 
-    filter_price = models.ForeignKey(Filter_price,on_delete=models.CASCADE)
 
 
 
@@ -66,9 +49,7 @@ class Images(models.Model):
 
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=200)
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+
 
 
 
